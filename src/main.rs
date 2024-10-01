88e9a8f4aca6213c27a6f59c9af46bd500adf5d3
kulapoo <kulaskulapoo@gmail.com>
2024-10-02 01:23:30 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,8 @@
-use actix_web::{web, App, HttpServer, Responder};
-use jujutsu_kaisen_api::controllers::health_check;
+use jujutsu_kaisen_api::startup;
+use std::net::TcpListener;
 
-async fn greet() -> impl Responder {
-    format!("Welcome to the Jujutsu Kaisen API!")
-}
-
-#[tokio::main] // Use Tokio as the async runtime
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/health-check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    startup::run(address)?.await
 }
