29787c882b4d4203fd0a1c4494aa029dfba5bf0f
kulapoo <kulaskulapoo@gmail.com>
2024-10-02 02:29:07 +0800

    rm for now, will set as baselin for boilerplate

--- migrations/20241001165853_create_characters_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE characters (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    status TEXT NOT NULL,
-    species TEXT,
-    gender TEXT,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
-);
--- src/domain/character.rs
+++
@@ -1,7 +0,0 @@
-pub struct Character {
-    pub id: i32,
-    pub name: String,
-    pub status: String,
-    pub species: String,
-    pub gender: String,
-}
